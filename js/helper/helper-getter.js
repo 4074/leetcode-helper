8ad559f6bc1efed303c251d035930dc1c8faac8b
邓文峰 <dwfn3398@corp.netease.com>
2016-09-09 16:33:21 +0800

    adapt to leetcode updates

--- js/helper/helper-getter.js
+++ js/helper/helper-getter.js
@@ -1,32 +1,32 @@
 + function(){
-	
+
 	var Getter = function(){};
-	
+
 	Getter.prototype.getQuestionMarkdown = function(){
 		var question = this.getQuestionInfo()
 		var content = this.clearContentMarkdown(this.transToMd(question.content))
-		var md = this.transToMd(question.title) + '\n\n' 
+		var md = this.transToMd(question.title) + '\n\n'
 			+ this.transToMd(question.info) + '\n\n'
 			+ content
-		
+
 		return md
 
 	}
-	
+
 	Getter.prototype.getQuestionInfo = function(){
 		var url = window.location.href
-		
+
 		var $title = $('.question-title h3')
 		var title = '<h3><a href="' + url + '">' + $title.html() + '</a></h3>'
-		
-		var info = $title.parent().next('.row').find('span').last().html()
-		
+
+		var info = $('.question-info li').last().html()
+
 		var $content = $('.question-content').clone()
 		$content.find('a').remove()
 		$content.find(':hidden').show()
 		$content.find('>div:not(.spoilers)').remove()
 		var content = $content.html()
-		
+
 		return {
 			url: url,
 			title: title,
@@ -34,7 +34,7 @@
 			content: content
 		}
 	}
-	
+
 	Getter.prototype.clearContentMarkdown = function(md){
 		md = md
 		.replace(/\*\*Credits[^\v]*/g, '')
@@ -42,13 +42,13 @@
 		.replace(/\<\/pre\>/g, '```')
 		.replace(/\<div[^\>]*\>/g, '')
 		.replace(/\<\/div\>/g, '')
-		
+
 		return md
 	}
-	
+
 	Getter.prototype.transToMd = function(html){
 		return html ? toMarkdown(html) : ''
 	}
-	
+
 	Helper.getter = new Getter()
 }()
