9b197fef75a62c529ec6d1fe01a1d3fbd08c3527
小峰 <dwfn3398@corp.netease.com>
2019-08-21 19:22:49 +0800

    Format document

--- js/helper/helper-getter.js
+++ js/helper/helper-getter.js
@@ -1,6 +1,6 @@
 +function () {
 
-    var Getter = function () {}
+  var Getter = function () { }
 
 	/**
 	 * Return markdown for question
@@ -11,22 +11,22 @@
 	 * @return {String}
 	 * @api public
 	 */
-    Getter.prototype.getQuestionMarkdown = function (title, url, $wrap) {
-        var isContest = url.indexOf('.com/contest/') >= 0
+  Getter.prototype.getQuestionMarkdown = function (title, url, $wrap) {
+    var isContest = url.indexOf('.com/contest/') >= 0
 
-        var question = isContest
-            ? this.getQuestionInfoForContest(title, url, $wrap)
-            : this.getQuestionInfoForProblem(title, url, $wrap)
+    var question = isContest
+      ? this.getQuestionInfoForContest(title, url, $wrap)
+      : this.getQuestionInfoForProblem(title, url, $wrap)
 
-        var markdown = this.translateToMarkdown(question.title) + '\n\n'
-            + this.translateToMarkdown(question.info) + '\n\n'
-            + this.clearContentMarkdown(this.translateToMarkdown(question.content)) + '\n\n'
+    var markdown = this.translateToMarkdown(question.title) + '\n\n'
+      + this.translateToMarkdown(question.info) + '\n\n'
+      + this.clearContentMarkdown(this.translateToMarkdown(question.content)) + '\n\n'
 
-        // Clear more \n
-		markdown = markdown.replace(/\n{4,}/g, '\n\n\n') + question.answer
-		
-        return markdown
-	}
+    // Clear more \n
+    markdown = markdown.replace(/\n{4,}/g, '\n\n\n') + question.answer
+
+    return markdown
+  }
 
 	/**
 	 * Get question info for problem page
@@ -38,39 +38,39 @@
 	 * @api private
 	 * 
 	 */
-	Getter.prototype.getQuestionInfoForProblem = function (title, url, $wrap) {
-		var $description = $('[data-key=description-content]')
-		// Title
-		title = '<h3><a href="' + url + '">' + title + '</a></h3>'
+  Getter.prototype.getQuestionInfoForProblem = function (title, url, $wrap) {
+    var $description = $('[data-key=description-content]')
+    // Title
+    title = '<h3><a href="' + url + '">' + title + '</a></h3>'
 
-		// Difficulty
-		var $difficulty = $description.find('div div div').eq(1).find('div').first()
-		var difficultyText = $difficulty.text()
-		// For CN UI
-		if (!difficultyText) {
-			difficultyText = $('[data-degree=easy],[data-degree=medium],[data-degree=hard]').first().text()
-		}
-		var info = 'Difficulty: **' + difficultyText + '**'
+    // Difficulty
+    var $difficulty = $description.find('div div div').eq(1).find('div').first()
+    var difficultyText = $difficulty.text()
+    // For CN UI
+    if (!difficultyText) {
+      difficultyText = $('[data-degree=easy],[data-degree=medium],[data-degree=hard]').first().text()
+    }
+    var info = 'Difficulty: **' + difficultyText + '**'
 
-		// Content
-		var content = this.getContentFromDOM(
-			this.findByClassName(this.findByClassName($description, 'div', 'description'), 'div', 'content')
-		)
+    // Content
+    var content = this.getContentFromDOM(
+      this.findByClassName(this.findByClassName($description, 'div', 'description'), 'div', 'content')
+    )
 
-		// Answer
-		var answer = this.getAnswerFromDOM(
-			$wrap.find('.CodeMirror-code').first(),
-			$wrap.find('.ant-select-selection-selected-value').first().text()
-		)
+    // Answer
+    var answer = this.getAnswerFromDOM(
+      $wrap.find('.CodeMirror-code').first(),
+      $wrap.find('.ant-select-selection-selected-value').first().text()
+    )
 
-		return {
-			url: url,
-			title: title,
-			info: info,
-			content: content,
-			answer: answer
-		}
-	}
+    return {
+      url: url,
+      title: title,
+      info: info,
+      content: content,
+      answer: answer
+    }
+  }
 
 	/**
 	 * Get question info for contest page
@@ -82,34 +82,34 @@
 	 * @api private
 	 * 
 	 */
-    Getter.prototype.getQuestionInfoForContest = function (title, url, $wrap) {
-        //Title in parameter is not right
-        title = $wrap.find('h3').text()
-        title = '<h3><a href="' + url + '">' + title + '</a></h3>'
+  Getter.prototype.getQuestionInfoForContest = function (title, url, $wrap) {
+    //Title in parameter is not right
+    title = $wrap.find('h3').text()
+    title = '<h3><a href="' + url + '">' + title + '</a></h3>'
 
-        // Info
-        var difficultyText = $wrap.find('div.contest-question-info.pull-right > ul > li:nth-child(5) > span').text()
-        var info = 'Difficulty: **' + difficultyText + '**'
+    // Info
+    var difficultyText = $wrap.find('div.contest-question-info.pull-right > ul > li:nth-child(5) > span').text()
+    var info = 'Difficulty: **' + difficultyText + '**'
 
-        // Content
-        var content = this.getContentFromDOM(
-			this.findByClassName($wrap, 'div', 'question-content')
-		)
+    // Content
+    var content = this.getContentFromDOM(
+      this.findByClassName($wrap, 'div', 'question-content')
+    )
 
-        // Answer
-        var answer = this.getAnswerFromDOM(
-			$wrap.find('.CodeMirror-code').first(),
-			$wrap.find('.Select-value-label').first().text()
-		)
+    // Answer
+    var answer = this.getAnswerFromDOM(
+      $wrap.find('.CodeMirror-code').first(),
+      $wrap.find('.Select-value-label').first().text()
+    )
 
-        return {
-            url: url,
-            title: title,
-            info: info,
-            content: content,
-            answer: answer
-        }
+    return {
+      url: url,
+      title: title,
+      info: info,
+      content: content,
+      answer: answer
     }
+  }
 
 	/**
 	 * Get question content from DOM
@@ -118,32 +118,31 @@
 	 * @return {String} content
 	 * @api private
 	 */
-	Getter.prototype.getContentFromDOM = function ($content) {
-		// Clone
-		$content = $content.clone()
+  Getter.prototype.getContentFromDOM = function ($content) {
+    // Clone
+    $content = $content.clone()
 
-		// Remove tranlation switch for leetcode-cn.com
-		var $translation = this.findByClassName($content, 'div', 'translation-tool')
-		if ($translation) {
-			$translation.remove()
-		}
-		$content.find('a').remove()
-		$content.find(':hidden').show()
-		$content.find('pre').each(function() {
-			var $pre = $(this)
-			$pre.html($pre.text())
-		})
-		// Clear extra span tag
-		$content.find('p').each(function() {
-			var $p = $(this), $children = $p.children()
-			if ($children.length === 1 && $children.get(0).tagName === 'SPAN') {
-				$p.html($children.get(0).innerHTML)
-			}
+    // Remove tranlation switch for leetcode-cn.com
+    var $translation = this.findByClassName($content, 'div', 'translation-tool')
+    if ($translation) {
+      $translation.remove()
+    }
+    $content.find('a').remove()
+    $content.find(':hidden').show()
+    $content.find('pre').each(function () {
+      var $pre = $(this)
+      $pre.html($pre.text())
+    })
+    // Clear extra span tag
+    $content.find('p').each(function () {
+      var $p = $(this), $children = $p.children()
+      if ($children.length === 1 && $children.get(0).tagName === 'SPAN') {
+        $p.html($children.get(0).innerHTML)
+      }
+    })
 
-		})
-
-		return $content.html()
-	}
+    return $content.html()
+  }
 
 	/**
 	 * Get question answer from DOM
@@ -153,21 +152,21 @@
 	 * @return {String} answer
 	 * @api private
 	 */
-	Getter.prototype.getAnswerFromDOM = function($answer, language) {
-		$answer = $answer.clone()
-		var answerLines = []
-		$answer.find('.CodeMirror-linenumber').remove()
-		$answer.find('.CodeMirror-line').each(function () {
-			var $line = $(this)
-			answerLines.push($line.text())
-		})
+  Getter.prototype.getAnswerFromDOM = function ($answer, language) {
+    $answer = $answer.clone()
+    var answerLines = []
+    $answer.find('.CodeMirror-linenumber').remove()
+    $answer.find('.CodeMirror-line').each(function () {
+      var $line = $(this)
+      answerLines.push($line.text())
+    })
 
-		return '#### Solution'
-		+ '\n\nLanguage: **' + language + '**'
-		+ '\n\n```'+ language.toLowerCase()+ '\n'
-			+ answerLines.join('\n')
-		+ '\n```'
-	}
+    return '#### Solution'
+      + '\n\nLanguage: **' + language + '**'
+      + '\n\n```' + language.toLowerCase() + '\n'
+      + answerLines.join('\n')
+      + '\n```'
+  }
 
 	/**
 	 * Find DOM by fuzzy class name
@@ -181,21 +180,21 @@
 	 * @return {jQueryDOM} $target
 	 * @api private
 	 */
-	Getter.prototype.findByClassName = function ($wrap, tag, name) {
-		var $els = $wrap.find(tag)
-		var $target
-		$els.each(function () {
-			if (!$target) {
-				var $el = $(this)
-				if (($el.attr('class') || '').indexOf(name) >= 0) {
-					$target = $el
-					return false
-				}
-			}
-		})
+  Getter.prototype.findByClassName = function ($wrap, tag, name) {
+    var $els = $wrap.find(tag)
+    var $target
+    $els.each(function () {
+      if (!$target) {
+        var $el = $(this)
+        if (($el.attr('class') || '').indexOf(name) >= 0) {
+          $target = $el
+          return false
+        }
+      }
+    })
 
-		return $target
-	}
+    return $target
+  }
 
 	/**
 	 * Translate html to markdown using to-markdown.js
@@ -204,9 +203,9 @@
 	 * @return {String} markdown
 	 * @api private
 	 */
-	Getter.prototype.translateToMarkdown = function (html) {
-		return html ? toMarkdown(html) : ''
-	}
+  Getter.prototype.translateToMarkdown = function (html) {
+    return html ? toMarkdown(html) : ''
+  }
 
 	/**
 	 * Clear markdown string for question content
@@ -215,15 +214,15 @@
 	 * @return {String} clean markdown
 	 * @api private
 	 */
-	Getter.prototype.clearContentMarkdown = function (markdown) {
-		return markdown.replace(/\*\*Credits[^\v]*/g, '')
-			.replace(/\<pre[^\>]*\>/g, '```\n')
-			.replace(/\<\/pre\>/g, '```')
-			.replace(/\<sup[^\>]*\>/g, '<sup>')
-			.replace(/\<div[^\>]*\>/g, '')
-			.replace(/\<\/div\>/g, '')
-	}
+  Getter.prototype.clearContentMarkdown = function (markdown) {
+    return markdown.replace(/\*\*Credits[^\v]*/g, '')
+      .replace(/\<pre[^\>]*\>/g, '```\n')
+      .replace(/\<\/pre\>/g, '```')
+      .replace(/\<sup[^\>]*\>/g, '<sup>')
+      .replace(/\<div[^\>]*\>/g, '')
+      .replace(/\<\/div\>/g, '')
+  }
 
-	Helper.getter = new Getter()
+  Helper.getter = new Getter()
 
 }()
--- js/main.js
+++ js/main.js
@@ -1,102 +1,102 @@
-$(function(){
-	var $body = $('body')
-	var isCN = window.location.host.indexOf('cn') >= 0
+$(function () {
+  var $body = $('body')
+  var isCN = window.location.host.indexOf('cn') >= 0
 
-	// Try times
-	var times = 0
-	var maxTryTimes = 20
-	
-	var questionTitle = ''
-	var currentUrl = ''
-	var hasBindCopy = false
+  // Try times
+  var times = 0
+  var maxTryTimes = 20
 
-	init()
+  var questionTitle = ''
+  var currentUrl = ''
+  var hasBindCopy = false
 
-	// Listen DOM modified, call the init method
-	window.addEventListener('DOMSubtreeModified', function() {
-		setTimeout(init, 1000)
-	})
+  init()
+
+  // Listen DOM modified, call the init method
+  window.addEventListener('DOMSubtreeModified', function () {
+    setTimeout(init, 1000)
+  })
 
 	/**
 	 * The main function.
 	 * Find out the title DOM of question, render copy button behind it.
 	 */
-	function init(){
-		// Avoid init repeatedly
-		if (window.location.href === currentUrl) return;
+  function init() {
+    // Avoid init repeatedly
+    if (window.location.href === currentUrl) return;
 
-		var $title = $('.question-title h3')
-		if ($title.length) {
-			$title = $title.parent()
-		} else {
-			if (!isCN) {
-				$title = $('[data-key=description-content] div div div').eq(0)
-			} else {
-				$title = $('h4')
-			}
-		}
+    var $title = $('.question-title h3')
+    if ($title.length) {
+      $title = $title.parent()
+    } else {
+      if (!isCN) {
+        $title = $('[data-key=description-content] div div div').eq(0)
+      } else {
+        $title = $('h4')
+      }
+    }
 
-		// LeetCode render question dom lazily.
-		// Therefore, set a timer to render copy button.
-		if ($title.length) {
-			if ($title.find('a.lch-btn-markdown').length === 0 || window.location.href !== currentUrl) {
-				currentUrl = window.location.href
-				var $c = $title.clone()
-				$c.find('a.lch-btn-markdown').remove()
-				questionTitle = $c.first().text()
-				renderCopyButton($title)
-			}
-		} else {
-			times += 1
-			if (times < maxTryTimes) {
-				setTimeout(init, 1000)
-			}
-		}
-	}
-	
+    // LeetCode render question dom lazily.
+    // Therefore, set a timer to render copy button.
+    if ($title.length) {
+      if ($title.find('a.lch-btn-markdown').length === 0 || window.location.href !== currentUrl) {
+        currentUrl = window.location.href
+        var $c = $title.clone()
+        $c.find('a.lch-btn-markdown').remove()
+        questionTitle = $c.first().text()
+        renderCopyButton($title)
+      }
+    } else {
+      times += 1
+      if (times < maxTryTimes) {
+        setTimeout(init, 1000)
+      }
+    }
+  }
+
 	/**
 	 * Render Copy button behind question title.
 	 * 
 	 * @param {jQueryDOM} $parent 
 	 */
-	function renderCopyButton($parent) {
-		$('a.lch-btn-markdown').remove()
-		var $btn = $('<a class="lch-btn-markdown hint--top" aria-label="Copy to clipboard" href="javascript:void(0);">').html('Copy for Markdown')
-		$btn.addClass(isCN ? 'lch-btn-cn' : 'lch-btn-en')
-		$btn.appendTo($parent)
-		bindCopyButton('.lch-btn-markdown')
-	}
-	
+  function renderCopyButton($parent) {
+    $('a.lch-btn-markdown').remove()
+    var $btn = $('<a class="lch-btn-markdown hint--top" aria-label="Copy to clipboard" href="javascript:void(0);">').html('Copy for Markdown')
+    $btn.addClass(isCN ? 'lch-btn-cn' : 'lch-btn-en')
+    $btn.appendTo($parent)
+    bindCopyButton('.lch-btn-markdown')
+  }
+
 	/**
 	 * Bind button click event, copy the markdown of question to clipboard.
 	 * 
 	 * @param {String} selector the css selector to find button
 	 */
-	function bindCopyButton(selector){
-		// Clipboard bind is for window.
-		// After display next question, it is already bind.
-		if (hasBindCopy) return;
+  function bindCopyButton(selector) {
+    // Clipboard bind is for window.
+    // After display next question, it is already bind.
+    if (hasBindCopy) return;
 
-		hasBindCopy = true
+    hasBindCopy = true
 
-		// Init Clipboard plugin
-		var clipboard = new Clipboard(selector, {
-			text: function(){
-				return Helper.getter.getQuestionMarkdown(questionTitle, window.location.href, $body)
-			}
-		})
-		
-		clipboard.on('success', function() {
-			var $btn = $(selector)
+    // Init Clipboard plugin
+    var clipboard = new Clipboard(selector, {
+      text: function () {
+        return Helper.getter.getQuestionMarkdown(questionTitle, window.location.href, $body)
+      }
+    })
 
-			// Set copied tips
-			$btn.attr("aria-label", 'Copied!')
+    clipboard.on('success', function () {
+      var $btn = $(selector)
 
-			// Reset tips
-			setTimeout(function() {
-				$btn.attr("aria-label", 'Copy to clipboard')
-			}, 2000)
-		})
-	}
+      // Set copied tips
+      $btn.attr("aria-label", 'Copied!')
+
+      // Reset tips
+      setTimeout(function () {
+        $btn.attr("aria-label", 'Copy to clipboard')
+      }, 2000)
+    })
+  }
 
 })
